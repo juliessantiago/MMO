802461430382c9355b7508f6026b76ef7604a3bc
Julie <Santiago.sjulie@gmail.com>
2021-02-01 22:59:21 -0300

    cria route e controller para criação de novo usuuário, cria verificação dos dados com express validator

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,5 +1,26 @@
 //controller de cadastro 
 
 module.exports.mostraCadastro = function (application, request, response){ // mostra página de cadastro
-    response.render('cadastro')//Não precisa colocar o caminho da view
+    response.render('cadastro', {validacao :{}})
+    //Não é necessário colocar .EJS 
+    /*estou passando aqui a variável validação porque seu conteúdo é testado na página, 
+    então é necessário que ela exista*/ 
+}
+module.exports.cadastrar = function (application, request, response){
+    let dados = request.body; 
+    //BodyParser 
+    
+    request.assert('nome', 'Campo nome não pode ser vazio').notEmpty(); //Express Validator
+    request.assert('user', 'Campo usuário não pode ser vazio').notEmpty(); 
+    request.assert('senha', 'Campo senha não pode ser vazio').notEmpty(); 
+    request.assert('casa', 'Você precisa escolher uma casa').notEmpty(); 
+
+    let erros = request.validationErrors(); 
+    
+    if(erros){
+        response.render('cadastro', {validacao : erros }) //renderiza a view cadastro enviando uma variável de erros 
+        return; //evitando que processo continue 
+    }else {
+        response.send('Cadastrando...')
+    }
 }
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -1,7 +1,12 @@
 //Route do cadastro 
+//recebe as URLs
 module.exports = function(application){ //application = objeto do servidor
-    application.get('/cadastro', function(request, response){
+    application.get('/cadastro', function(request, response){ //chama a página de cadastro
         console.log('chamando controller de cadastro')
        application.app.controllers.cadastro.mostraCadastro(application, request, response)
+    })
+
+    application.post('/cadastrar', function(request, response){ //recupera dados do formulário de cadastro
+        application.app.controllers.cadastro.cadastrar(application, request, response)
     })
 }
